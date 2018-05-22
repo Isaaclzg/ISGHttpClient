7232412d00d15162a65d9f6186f4615c7b43b0b8
isaac_gang <isaac_gang@163.com>
2018-05-22 16:41:48 +0800

    0.3

--- ISGHttpClient/ISGNetworkConfig.h
+++ ISGHttpClient/ISGNetworkConfig.h
@@ -5,10 +5,8 @@
 //  Created by Isaac on 2018/5/22.
 //  Copyright © 2018年 Isaac. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
-
-
+#ifndef ISGNetworkConfig_h
+#define ISGNetworkConfig_h
 /*!
  * `1`:测试环境
  * `0`:生产环境
@@ -33,7 +31,7 @@
 #ifdef DEBUG
 #define ISGLog(fmt, ...) NSLog((@ "[行号:%d] \n" "[函数名:%s]\n"  fmt),  __LINE__, __FUNCTION__, ##__VA_ARGS__);
 #else
-#define NSLog(...);
+#define ISGLog(...);
 #endif
 
 
@@ -44,7 +42,8 @@
 #define ISGLogRequestSuccess(urlStr, method, paramDict, result) ISGLog(@"\n%@\n请求地址:\n%@\n请求方式:\n%@\n请求参数:\n%@\n%@\n请求成功,返回数据:\n%@\n%@", (kLine1), (urlStr), (method), (paramDict), (kLine2), (result), kLine1)
 
 // 请求失败输出的信息
-#define ISGLogRequestFailure(urlStr, method, paramDict, error) NSLog(@"\n%@\n请求地址:\n%@\n请求方式:\n%@\n请求参数:\n%@\n%@\n请求失败,失败原因:\n%@\n%@\n%@", (kLine1), (urlStr), (method), (paramDict), (kLine2), (error.localizedDescription), (error.userInfo), kLine2)
+#define ISGLogRequestFailure(urlStr, method, paramDict, error) ISGLog(@"\n%@\n请求地址:\n%@\n请求方式:\n%@\n请求参数:\n%@\n%@\n请求失败,失败原因:\n%@\n%@\n%@", (kLine1), (urlStr), (method), (paramDict), (kLine2), (error.localizedDescription), (error.userInfo), kLine2)
 
 // JSON序列化
 #define ISGJson(data) [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil]
+#endif 
